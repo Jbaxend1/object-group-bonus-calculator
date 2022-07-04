2858fdffb1b1dfb6512211b9a2e43c9b9c82a4e1
Jonathon Baxendell <j.bax626@gmail.com>
2022-07-03 20:24:11 -0500

    individual bonus logic

--- client.js
+++ client.js
@@ -39,4 +39,53 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+
+/**
+ * 
+ * @param {*} employeeObj 
+ * @param {*} employees 
+ */
+
+
+ function calculateBonus(employeeObj, cohortArray){
+
+  
+  let bonusPercent = 0;
+ 
+  console.log('Calculating bonus for', employeeObj.name);
+  if (employeeObj.rating <= 2) {
+     console.log('No bonus', employeeObj.name);
+      bonusPercent = 0;
+  } else if (employeeObj.reviewRating === 3) {
+      bonusPercent += 0.04;
+  } else if (employeeObj.reviewRating === 4) {
+      bonusPercent += 0.06;
+  } else if (employeeObj.reviewRating === 5) {
+      bonusPercent += 0.1;
+  } 
+
+  if(employeeObj.employeeNumber.length === 4){
+      bonusPercent += 0.05;}
+
+  if(parseInt(employeeObj.annualSalary) > 65000){
+  bonusPercent -= 0.01;}
+  
+  if(bonusPercent > .13){
+    bonusPercent = .13;
+  } else if( bonusPercent <= 0){
+    bonusPercent = 0;
+  }
+
+  let bonusAmount = parseInt(employeeObj.annualSalary) * bonusPercent;
+  let totalComp = Math.ceil(parseInt(employeeObj.annualSalary) + bonusAmount);
+
+  return {
+    
+    name: employeeObj.name,
+    bonusPercent: bonusPercent,
+    bonusAmount: bonusAmount,
+    totalComp: totalComp,
+ }
+};
+
+ console.log(calculateBonus(employees[2]));
